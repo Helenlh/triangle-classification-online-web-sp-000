4be67022218ada622cc82922176b197f9296dda2
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-16 19:49:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,7 +28,7 @@ end
     [a, b, c].each do |side|
       real_triangle << false if side <= 0 
     raise TriangleError if real_triangle.include?(false)
-   
+    raise TriangleError if !side 
     rescue TriangleError => error
     puts error.message 
     end
